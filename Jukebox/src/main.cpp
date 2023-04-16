b8f1b0b45bad62107c9d392ccc13a5fb54e9b129
MarioLndz <mariolindez@gmail.com>
2023-04-16 17:59:15 +0200

    Initial commit

---
+++ Jukebox/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// https://code-with-me.global.jetbrains.com/BIS468WfGnbPeo8ihYUQIw#p=CL&fp=3D99BCF1D2245B0D4CAE6E97EA5DF5F10A66786425E8151046ECE1374CDFC52B
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
