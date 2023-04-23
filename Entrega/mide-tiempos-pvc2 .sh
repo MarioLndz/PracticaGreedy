425b0b702b41409544871a5d88b2cd11b23a13fb
Juan <juanayuso@correo.ugr.es>
2023-04-23 19:33:12 +0200

    TRABAJO

---
+++ Entrega/mide-tiempos-pvc2 .sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#echo "" >> salida.dat
+printf "" > pvc2.dat
+
+i=50000
+while [ "$i" -le 1250000 ]
+do
+	# Generamos los puntos	
+	./generador-pvc $i
+  	
+	# Ejecutamos los puntos
+	./pvc2 data.txt >> pvc2.dat
+
+	echo "Terminado $i" 
+	
+	i=$(( $i + 50000 ))
+done
